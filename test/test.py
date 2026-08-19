85850b59b776c272f77394ce56cba70dac57da5b
Alberto Flores <aljfloresh@csu.fullerton.edu>
2026-08-19 15:58:25 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 15.152, unit="ps")
+    clock = Clock(dut.clk, 15152, unit="ps")
     cocotb.start_soon(clock.start())
 
     # Reset
